38ccd50c0b15a4e74be17be333a647674d9ec9c0
Gary Russell <grussell@pivotal.io>
2016-04-06 10:33:58 -0400

    Port Rabbit Source/Sink

---
+++ apps/rabbit-sink-rabbit/src/main/java/org/springframework/cloud/stream/app/rabbit/sink/RabbitSinkRabbitApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.cloud.stream.app.rabbit.sink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitSinkRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitSinkRabbitApplication.class, args);
+	}
+}
