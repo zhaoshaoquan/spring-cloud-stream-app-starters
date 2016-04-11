3a065d3c35674d6fbf804f21d36daaa932553cfc
Gary Russell <grussell@pivotal.io>
2016-04-11 14:38:50 -0400

    WebSocket Sink

---
+++ websocket/spring-cloud-starter-stream-sink-websocket/src/main/java/org/springframework/cloud/stream/app/websocket/sink/WebsocketSinkProperties.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2014-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.springframework.cloud.stream.app.websocket.sink;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import io.netty.handler.logging.LogLevel;
+
+/**
+ * Command line arguments available for {@link WebsocketSink}.
+ *
+ * @author Oliver Moser
+ * @author Gary Russell
+ */
+@ConfigurationProperties
+public class WebsocketSinkProperties {
+
+	public static final String DEFAULT_LOGLEVEL = LogLevel.WARN.toString();
+
+	public static final String DEFAULT_PATH = "/websocket";
+
+	public static final int DEFAULT_THREADS = 1;
+
+	public static final int DEFAULT_PORT = 9292;
+
+	/**
+	 * whether or not to create a {@link io.netty.handler.ssl.SslContext}
+	 */
+	boolean ssl;
+
+	/**
+	 * the websocketPort on which the Netty server listens. Default is <tt>9292</tt>
+	 */
+	int websocketPort = DEFAULT_PORT;
+
+	/**
+	 * the number of threads for the Netty {@link io.netty.channel.EventLoopGroup}. Default is <tt>1</tt>
+	 */
+	int threads = DEFAULT_THREADS;
+
+	/**
+	 * the websocketLoglevel for netty channels. Default is <tt>WARN</tt>
+	 */
+	String websocketLoglevel = DEFAULT_LOGLEVEL;
+
+	/**
+	 * the websocketPath on which a WebsocketSink consumer needs to connect. Default is <tt>/websocket</tt>
+	 */
+	String websocketPath = DEFAULT_PATH;
+
+	public boolean isSsl() {
+		return ssl;
+	}
+
+	public void setSsl(boolean ssl) {
+		this.ssl = ssl;
+	}
+
+	public int getWebsocketPort() {
+		return websocketPort;
+	}
+
+	public void setWebsocketPort(int websocketPort) {
+		this.websocketPort = websocketPort;
+	}
+
+	public int getThreads() {
+		return threads;
+	}
+
+	public void setThreads(int threads) {
+		this.threads = threads;
+	}
+
+	public String getWebsocketLoglevel() {
+		return websocketLoglevel;
+	}
+
+	public void setWebsocketLoglevel(String websocketLoglevel) {
+		this.websocketLoglevel = websocketLoglevel;
+	}
+
+	public String getWebsocketPath() {
+		return websocketPath;
+	}
+
+	public void setWebsocketPath(String websocketPath) {
+		this.websocketPath = websocketPath;
+	}
+
+}
---
+++ websocket/spring-cloud-starter-stream-sink-websocket/src/main/java/org/springframework/cloud/stream/app/websocket/sink/WebsocketSinkServer.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2014-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.springframework.cloud.stream.app.websocket.sink;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * Bootstraps a Netty server using the {@link WebsocketSinkServerInitializer}. Also adds
+ * a {@link LoggingHandler} and uses the <tt>websocketLoglevel</tt>
+ * from {@link WebsocketSinkProperties#websocketLoglevel}.
+ *
+ * @author Oliver Moser
+ * @author Gary Russell
+ */
+public class WebsocketSinkServer {
+
+	private static final Log logger = LogFactory.getLog(WebsocketSinkServer.class);
+
+	static final List<Channel> channels = Collections.synchronizedList(new ArrayList<Channel>());
+
+	@Autowired
+	WebsocketSinkProperties properties;
+
+	@Autowired
+	WebsocketSinkServerInitializer initializer;
+
+	private EventLoopGroup bossGroup;
+
+	private EventLoopGroup workerGroup;
+
+	private int port;
+
+	public int getPort() {
+		return this.port;
+	}
+
+	@PostConstruct
+	public void init() {
+		bossGroup = new NioEventLoopGroup(properties.getThreads());
+		workerGroup = new NioEventLoopGroup();
+	}
+
+	@PreDestroy
+	public void shutdown() {
+		bossGroup.shutdownGracefully();
+		workerGroup.shutdownGracefully();
+	}
+
+	public void run() throws InterruptedException {
+		NioServerSocketChannel channel = (NioServerSocketChannel) new ServerBootstrap().group(bossGroup, workerGroup)
+			.channel(NioServerSocketChannel.class)
+			.handler(new LoggingHandler(nettyLogLevel()))
+			.childHandler(initializer)
+			.bind(properties.getWebsocketPort())
+			.sync()
+			.channel();
+		this.port = channel.localAddress().getPort();
+		dumpProperties();
+	}
+
+	private void dumpProperties() {
+		logger.info("███████████████████████████████████████████████████████████");
+		logger.info("                >> websocket-sink config <<                ");
+		logger.info("");
+		logger.info(String.format("websocketPort:     %s", this.port));
+		logger.info(String.format("ssl:               %s", this.properties.isSsl()));
+		logger.info(String.format("websocketPath:     %s", this.properties.getWebsocketPath()));
+		logger.info(String.format("websocketLoglevel: %s", this.properties.getWebsocketLoglevel()));
+		logger.info(String.format("threads:           %s", this.properties.getThreads()));
+		logger.info("");
+		logger.info("████████████████████████████████████████████████████████████");
+	}
+
+	//
+	// HELPERS
+	//
+	private LogLevel nettyLogLevel() {
+		return LogLevel.valueOf(properties.getWebsocketLoglevel().toUpperCase());
+	}
+
+
+}
