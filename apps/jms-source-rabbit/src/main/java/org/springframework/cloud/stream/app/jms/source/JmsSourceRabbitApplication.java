dfbd5052a1a310f116ddf1b36c784431140d5c1b
Gary Russell <grussell@pivotal.io>
2016-04-11 13:28:29 -0400

    JMS Source

---
+++ apps/jms-source-rabbit/src/main/java/org/springframework/cloud/stream/app/jms/source/JmsSourceRabbitApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.cloud.stream.app.jms.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsSourceRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsSourceRabbitApplication.class, args);
+	}
+}
