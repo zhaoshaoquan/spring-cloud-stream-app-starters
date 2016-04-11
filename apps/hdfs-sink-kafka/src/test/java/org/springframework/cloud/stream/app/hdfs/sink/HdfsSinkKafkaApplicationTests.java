7899ad81a641e7a4ceac7b50d5243e621d17bc47
Soby Chacko <schacko@pivotal.io>
2016-04-11 18:24:33 -0400

    HDFS sink

---
+++ apps/hdfs-sink-kafka/src/test/java/org/springframework/cloud/stream/app/hdfs/sink/HdfsSinkKafkaApplicationTests.java
@@ -0,0 +1,18 @@
+package org.springframework.cloud.stream.app.hdfs.sink;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@Ignore
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HdfsSinkKafkaApplication.class)
+public class HdfsSinkKafkaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
