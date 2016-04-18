da1ac850a30c64c5b211afef4431fb5c54049c92
Gary Russell <grussell@pivotal.io>
2016-04-18 16:07:20 -0400

    Scriptable App Tests

--- apps/groovy-filter-processor-kafka/src/test/java/org/springframework/cloud/stream/app/groovy/filter/processor/kafka/GroovyFilterProcessorKafkaApplicationTests.java
+++ apps/groovy-filter-processor-kafka/src/test/java/org/springframework/cloud/stream/app/groovy/filter/processor/kafka/GroovyFilterProcessorKafkaApplicationTests.java
@@ -22,7 +22,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = GroovyFilterProcessorKafkaApplication.class)
+@SpringApplicationConfiguration(classes = {
+		org.springframework.cloud.stream.app.test.script.ScriptableTestConfiguration.class,
+		GroovyFilterProcessorKafkaApplication.class })
 public class GroovyFilterProcessorKafkaApplicationTests {
 
 	@Test
--- apps/scriptable-transform-processor-kafka/src/test/java/org/springframework/cloud/stream/app/scriptable/transform/processor/kafka/ScriptableTransformProcessorKafkaApplicationTests.java
+++ apps/scriptable-transform-processor-kafka/src/test/java/org/springframework/cloud/stream/app/scriptable/transform/processor/kafka/ScriptableTransformProcessorKafkaApplicationTests.java
@@ -22,7 +22,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ScriptableTransformProcessorKafkaApplication.class)
+@SpringApplicationConfiguration(classes = {
+		org.springframework.cloud.stream.app.test.script.ScriptableTestConfiguration.class,
+		ScriptableTransformProcessorKafkaApplication.class })
 public class ScriptableTransformProcessorKafkaApplicationTests {
 
 	@Test
