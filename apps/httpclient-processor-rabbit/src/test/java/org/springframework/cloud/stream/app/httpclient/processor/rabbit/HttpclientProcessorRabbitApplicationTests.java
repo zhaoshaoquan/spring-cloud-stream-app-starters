0c20a85fb7342f58431c401b8789b2842dd32ba7
Gary Russell <grussell@pivotal.io>
2016-04-18 16:42:05 -0400

    HTTP Client Processor App Tests

--- apps/httpclient-processor-rabbit/src/test/java/org/springframework/cloud/stream/app/httpclient/processor/rabbit/HttpclientProcessorRabbitApplicationTests.java
+++ apps/httpclient-processor-rabbit/src/test/java/org/springframework/cloud/stream/app/httpclient/processor/rabbit/HttpclientProcessorRabbitApplicationTests.java
@@ -22,7 +22,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = HttpclientProcessorRabbitApplication.class)
+@SpringApplicationConfiguration(classes = {
+		org.springframework.cloud.stream.app.test.http.HttpClientProcessorTestConfiguration.class,
+		HttpclientProcessorRabbitApplication.class })
 public class HttpclientProcessorRabbitApplicationTests {
 
 	@Test
