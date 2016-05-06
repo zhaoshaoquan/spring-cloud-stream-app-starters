1fe9b829ef00df99df1c54993a3413c80bb1f22f
Soby Chacko <schacko@pivotal.io>
2016-05-06 16:54:24 -0400

    Fix typo in generateApps script

--- generateApps.sh
+++ generateApps.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 
-#execute mvn plugin with app geneartion
+#execute mvn plugin with app generation
 mvn clean install scs:generate-app -pl :spring-cloud-stream-app-generator
